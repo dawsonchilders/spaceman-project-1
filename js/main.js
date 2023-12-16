8f25d02eac0362b25393313335b6dc0c9d8ab770
dawsonchilders <dawsonchilders@gmail.com>
2023-12-16 10:39:52 -0800

    added markup for all needed functions to layout needs of game

--- js/main.js
+++ js/main.js
@@ -1,16 +1,17 @@
 /*----- constants -----*/
+/*--ADD REST OF WORDS WHEN GAME IS FINISHED!!!--*/
 const words = ["father", "mother", "chicken",];
 
   /*----- state variables -----*/
-let answer;
-let guessed;
-let charArray;
-let lives;
-
+  let answer;
+  let guessed;
+  let charArray;
+  let lives;
 
 
 /*----- cached elements  -----*/
-const livesElem = document.getElementById('lives');
+
+
 
 /*----- event listeners -----*/
 
@@ -18,30 +19,65 @@ const livesElem = document.getElementById('lives');
 /*----- functions -----*/
 init();
 
+// Initialize function to set up the initial state
+
 function init() {
   answer = '';
   guessed = [];
-  let charArray = [];
   lives = 6;
+  renderWord();
   render();
 }
 
-
-function render() {
-  renderWord();
-  updateLives();
-  guessedWord();
-  
-}
-
+// Function to pick a random word
 
 function renderWord() {
   answer = words[Math.floor(Math.random() * words.length)];
-  randomWord = answer.split('');
+  charArray = answer.split('');
 }
 
-function updateLives() {
-  livesElem.innerHTML = `You have <span>${lives}</span> ${lives === 1 ? 'life' : 'lives'} left${lives === 0 ? '!' : ''}`;
+
+// Set up keyboard listeners
+
+
+
+
+
+// Process a guessed letter
+
+
+
+
+
+// Render the current state to the DOM
+
+function render() {
+  renderWord();
 }
+
+
+
+// Update the lives
+
+
+
+
+
+
+// Get the displayed word
+
+
+
+
+
+// Check for win/lose
+
+
+
+
+// Handle end of game
+
+
+
 
 
