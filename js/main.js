14cec1ca7549e00d10c2caf5f301f09783336a19
dawsonchilders <dawsonchilders@gmail.com>
2023-12-17 10:51:16 -0800

    functions: chooseWord, keyBoardListener, enableAllKeys - cleared CSS

--- js/main.js
+++ js/main.js
@@ -1,101 +1,60 @@
 /*----- constants -----*/
 /*--ADD REST OF WORDS WHEN GAME IS FINISHED!!!--*/
-const words = ["father", "mother", "chicken",];
+const words = ["father", "mother", "chicken"];
 
-  /*----- state variables -----*/
-  let answer;
-  let guessed;
-  let charArray;
-  let lives;
-
+/*----- state variables -----*/
+let answer;
+let guessedLetters;
+let charArray;
+let lives;
+let isGameActive;
 
 /*----- cached elements  -----*/
 const livesElem = document.getElementById('lives');
 const wordElem = document.getElementById('word');
 const keyElem = document.getElementById('keyboard');
-const msgBox = document.getElementById('endGameMessage')
-
-
-
-/*----- event listeners -----*/
+const msgBox = document.getElementById('endGameMessage');
 
 
 /*----- functions -----*/
 init();
 
-// Initialize function to set up the initial state
-
 function init() {
-  answer = '';
-  guessed = [];
-  charArray = [];
-  lives = 6;
-  renderWord();
-  keyboardListener();
-
-  render();
+    answer = '';
+    guessedLetters = [];
+    charArray = [];
+    lives = 6;
+    isGameActive = true;
+    chooseWord();
+    keyboardListener();
+    enableAllKeys();
+    render();
 }
 
-// Render the current state to the DOM
-render()
+
+
 function render() {
-  
+    
 }
 
-
-
-// Function to pick a random word
-
-function renderWord() {
-  answer = words[Math.floor(Math.random() * words.length)];
-  charArray = answer.split('');
+function chooseWord() {
+    answer = words[Math.floor(Math.random() * words.length)];
+    charArray = answer.split('');
 }
-
-
-// Set up keyboard listeners
 
 function keyboardListener() {
   keyElem.addEventListener('click', function(evt) {
-    if (evt.target.matches('.key')) {
-      renderGuess(evt.target.textContent);
-      evt.target.disabled = true;
-    }
-  })
+      if (isGameActive && evt.target.matches('.key')) {
+          handleGuess(evt.target.textContent);
+          evt.target.disabled = true;
+      }
+  });
+}
+
+function enableAllKeys() {
+    document.querySelectorAll('#keyboard .key').forEach(key => key.disabled = false);
 }
 
 
-// Process a guessed letter
-
-function renderGuess(letter) {
-  if (!guessed.includes(letter.toLowerCase())) {
-    guessed.push(letter.toLowerCase());
-    if (!answer.includes(letter.toLowerCase())) {
-      lives --;
-    }
-    render();
-  }
-}
-
-
-// Get displayed word
-
-function getWord() {
-  return charArray.map(letter => guessed.includes(letter.toLowerCase()) ? letter.toLowerCase() : '_').join(' ');
-} 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    
 
