a4bd80f4bb5f5a20b8fdae8189a4a2bb406843a8
dawsonchilders <dawsonchilders@gmail.com>
2023-12-17 19:24:13 -0800

    game built, partially working. issues: keys don't deactivate after win/loss, loss message works, win message does not

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ let answer;
 let guessedLetters;
 let charArray;
 let lives;
-let isGameActive;
 
 /*----- cached elements  -----*/
 const livesElem = document.getElementById('lives');
@@ -24,37 +23,80 @@ function init() {
     guessedLetters = [];
     charArray = [];
     lives = 6;
-    isGameActive = true;
-    chooseWord();
     keyboardListener();
-    enableAllKeys();
     render();
+    renderWord();
 }
 
 
 
 function render() {
-    
+    renderLives();
+    updateDisplayedWord();
+    checkForWin();
+    renderKeys();
 }
 
-function chooseWord() {
+function renderWord() {
     answer = words[Math.floor(Math.random() * words.length)];
     charArray = answer.split('');
 }
 
 function keyboardListener() {
   keyElem.addEventListener('click', function(evt) {
-      if (isGameActive && evt.target.matches('.key')) {
+      if (evt.target.matches('.key')) {
           handleGuess(evt.target.textContent);
           evt.target.disabled = true;
       }
   });
 }
 
-function enableAllKeys() {
+function renderKeys() {
     document.querySelectorAll('#keyboard .key').forEach(key => key.disabled = false);
 }
 
+function handleGuess(letter) {
+    if (!guessedLetters.includes(letter.toLowerCase())) {
+        guessedLetters.push(letter.toLowerCase());
+        if (!answer.includes(letter.toLowerCase())) {
+            lives--;
+        }
+        render();
+    }
+}
 
+function renderLives() {
+    livesElem.innerHTML = `Lives: ${lives}`;
+    if (lives === 0) {
+        revealAnswer();
+        showEndGameMessage(false);
+    }
+}
+
+function updateDisplayedWord() {
+  wordElem.textContent = charArray.map(letter => guessedLetters.includes(letter) ? letter.toLowerCase() : '_').join(' ');
+}
+
+function checkForWin() {
+if (charArray.every(letter => guessedLetters.includes(letter))) {
+    showEndGameMessage(true);
+}
+}
     
+function showEndGameMessage(won) {
+  const messageText = won ? 'Congratulations, you won!' : `You Lose, the word was: ${answer}`;
+  const endGameText = document.getElementById('endGameText');
+  endGameText.textContent = messageText;
+  msgBox.style.display = 'block';
+
+  document.getElementById('restartButton').addEventListener('click', function() {
+      msgBox.style.display = 'none';
+
+      init();
+  });
+}
+
+function revealAnswer() {
+  wordElem.textContent = answer;
+}
 
